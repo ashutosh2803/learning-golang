2cab8ea5bb2e3af67f4a7a6f6ad5511284fcfec2
Ashutosh Kumar <ashuisalive@gmail.com>
2026-01-07 01:31:23 +0530

    types and structs covered

--- main.go
+++ main.go
@@ -1,40 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"time"
 )
 
-// package level variable
-var name string = "Ashutosh"
+type User struct {
+	FirstName string
+	LastName  string
+	Age       int
+	BirthDate time.Time
+}
 
 func main() {
-	fmt.Println("Hello World!")
-
-	var whatToSay string
-	var i int
-
-	whatToSay = "Goodbye, Cruel World!"
-	i = 7
-
-	fmt.Println(whatToSay)
-	fmt.Println("i is set to", i)
-
-	whatWasSaid, theOtherThingThatWasSaid := saySomething()
-	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid)
-
-	var color string
-	color = "Green"
-	log.Println("color is set to", color)
-	changeUsingPointer(&color)
-	log.Println("after func call, color is set to", color)
+	createUser()
 }
 
-func saySomething() (string, string) {
-	return "Something", "else"
-}
-
-func changeUsingPointer(s *string) {
-	newValue := "Red"
-	*s = newValue
+func createUser() {
+	user := User{
+		FirstName: "Ashutosh",
+		LastName:  "Kumar",
+		Age:       27,
+	}
+	log.Println(user.FirstName, user.LastName, user.Age, user.BirthDate)
 }
