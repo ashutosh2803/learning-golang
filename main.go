b69beb985c5edb3095a690363a99665b1c3c63f6
Ashutosh Kumar <ashuisalive@gmail.com>
2026-01-08 16:02:04 +0530

    added birthdate

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 )
@@ -25,5 +24,4 @@ func createUser() {
 		BirthDate: time.Date(1998, time.March, 28, 0, 0, 0, 0, time.UTC),
 	}
 	log.Println(user.FirstName, user.LastName, user.Age, user.BirthDate)
-	fmt.Println("ends")
 }
